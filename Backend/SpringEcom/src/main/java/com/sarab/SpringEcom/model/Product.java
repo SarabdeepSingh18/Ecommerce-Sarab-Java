8b6ee3cf100d56043e1329795f21cacd72b0fb95
Sarabdeep <noorraikhi18@gmail.com>
2026-01-28 12:43:06 +0530

    Postgres Integrated,model package started

---
+++ Backend/SpringEcom/src/main/java/com/sarab/SpringEcom/model/Product.java
@@ -0,0 +1,34 @@
+package com.sarab.SpringEcom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity //to map with a table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+
+
+}
